ee817acc4898531a4b97c5ab6d289d79ebeb7271
細川大輔 <hosokawadaisuke@hosokawadaisukenoMacBook-Pro.local>
2021-08-10 21:53:42 +0900

    manage.pyの解説をコメント

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
-#flaskrというアプリを起動して実行するためのファイル。
-#appは__init__.pyにてflaskモジュールのFlaskクラスから生成したインスタンス。
+#flaskrというアプリを起動して実行するためのファイル
+#appは__init__.pyにてflaskモジュールのFlaskクラスから生成したインスタンス
 from flaskr import app 
-#ローカルサーバーを起動し、アプリを立ち上げるためのメソッド。
+#ローカルサーバーを起動し、アプリを立ち上げるためのメソッド
 app.run(host='127.0.0.1', port=8080, debug=True)
