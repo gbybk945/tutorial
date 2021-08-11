ff0aef10d890a1fd01bd6786ed3ca89e3393cf35
細川大輔 <hosokawadaisuke@hosokawadaisukenoMacBook-Pro.local>
2021-08-12 06:41:38 +0900

    解説コメントを追加

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,16 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#ブログアプリを作成するため、データベースを仕様できるようにする初期設定
 
-app = Flask(__name__)
-app.config.from_object('flaskr.config')
+#flaskのモジュール（Flaskクラス）をインポート
+from flask import Flask　
+#データベースとやりとりをするライブラリをインポート
+#SQLの文を書かずにSQLAlchemyのライブラリが持っている関数を利用する事で代わりにSQL文の生成が可能。
+from flask_sqlalchemy import SQLAlchemy　
 
-db = SQLAlchemy(app)
+app = Flask(__name__)　#Flaskのインスタンスを生成して変数appに割り当て
+app.config.from_object('flaskr.config')　#pythonオブジェクトであるflaskr.configを読み込み、appの設定（config.pyで設定）を追加
 
-import flaskr.views
+#SQLAlchemyのインスタンスを生成し変数dbに割り当て。
+#引数にFlaskのインスタンス（app）を渡すことで、FlaskでSQLAlchemyを利用してデータベースを扱うようにできる。
+db = SQLAlchemy(app)　
+
+import flaskr.views　#views.pyモジュールをインポート
